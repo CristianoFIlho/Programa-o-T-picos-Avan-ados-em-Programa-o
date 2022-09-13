60bd3a4a36cc198d01e9d975ee852a0d8ff9ce66
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2022-09-13 13:54:55 -0300

    update python

---
+++ atividade07.py
@@ -0,0 +1,50 @@
+# Entrada de dados
+sentenca = input('Digite a sentença: ')
+janela = input('Digite a janela: ')
+
+# Processamento
+for i in range(len(sentenca) - len(janela) + 1):
+    if sentenca[i:i+len(janela)] == janela:
+        print('Padrão encontrado na posição', i)
+
+# Saída de dados
+print('Fim do programa')
+
+
+
+# Entrada de dados
+sentenca = 'YCTABWATAB'
+janela = 'AB'
+
+# Processamento
+for i in range(len(sentenca) - len(janela) + 1):
+    if sentenca[i:i+len(janela)] == janela:
+        print('Padrão encontrado na posição', i)
+
+# Saída de dados
+print('Fim do programa')
+
+
+
+# Entrada de dados
+sentenca = 'YCTABWATAB'
+janela = 'AB'
+
+# Processamento
+for i in range(len(sentenca) - len(janela) + 1):
+    if sentenca[i:i+len(janela)] == janela:
+        print('Padrão encontrado na posição', i)
+
+# Saída de dados
+print('Fim do programa')
+
+
+
+# Entrada de dados
+sentenca = 'YCTABWATAB'
+janela = 'AB'
+
+# Processamento
+for i in range(len(sentenca) - len(janela) + 1):
+    if sentenca[i:i+len(janela)] == janela:
+        print('Padrão encontrado na posição',
